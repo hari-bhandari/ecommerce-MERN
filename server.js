459b0a880214a34ceb35a723b9360fa4152a6439
Hari Bhandari <2012bhandari.ha@gmail.com>
2020-09-09 14:48:27 +0100

    initilised server using express

---
+++ server.js
@@ -0,0 +1,22 @@
+const express=require('express')
+// const path=require('path')
+// const cors=require('cors')
+// const dotenv=require('dotenv')
+////////////////////////////////
+const app=express()
+////////////////////////////////
+app.set('trust proxy',true);
+// dotenv.config({path:'./config/config.env'})
+const PORT= process.env.PORT||5000
+// app.use(cors())
+
+
+
+const server=app.listen(PORT,console.log(`server running in Production mode on port ${PORT}`))
+
+
+//handle unhandled promised rejections
+process.on('unhandledRejection',(err,promise)=>{
+    console.log(`error:${err.message}`)
+    server.close(()=>process.exit(1))
+});

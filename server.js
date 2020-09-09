19826cded3ea4f9633715e430fa69517bed8b21e
Hari Bhandari <2012bhandari.ha@gmail.com>
2020-09-09 14:51:19 +0100

    implemented helmet

--- server.js
+++ server.js
@@ -1,16 +1,19 @@
 const express=require('express')
 // const path=require('path')
+const helmet=require('helmet')
 const cors=require('cors')
 const dotenv=require('dotenv')
 ////////////////////////////////
 const app=express()
+
 ////////////////////////////////
 app.set('trust proxy',true);
 dotenv.config({path:'./config/config.env'})
 const PORT= process.env.PORT||5000
 //enable cors
 app.use(cors())
-
+//helmet for security headers
+app.use(helmet())
 
 
 const server=app.listen(PORT,console.log(`server running in Production mode on port ${PORT}`))

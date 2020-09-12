169431b5a2ddea4cff3bdc16fac14f6cdb893b53
Hari Bhandari <2012bhandari.ha@gmail.com>
2020-09-12 11:32:15 +0100

    added register

--- controllers/auth.js
+++ controllers/auth.js
@@ -2,7 +2,23 @@ const User=require('../models/User');
 const asyncHandler=require('../middleware/async');
 const ErrorResponse=require('../utils/errorResponse');
 const crypto=require('crypto')
+//@desc register a user
+//@route POST /api/v1/auth/register
+//@access Public
+exports.register=asyncHandler(async (req,res,next)=>{
+    const {name,email,password,role}=req.body;
 
+    //Create user
+    const user=await User.create({
+        name,
+        email,
+        password,
+        role
+    });
+    //Create web token and send it
+    sendTokenResponse(user,200,res)
+
+})
 //get token from model,create cookie and send
 // response
 const sendTokenResponse=(user,statusCode,res)=>{

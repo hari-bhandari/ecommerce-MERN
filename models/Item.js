c877b4153a1e2dee76bad03e4d259059a1345d08
Hari Bhandari <2012bhandari.ha@gmail.com>
2020-09-12 11:05:34 +0100

    added item model

--- models/Item.js
+++ models/Item.js
@@ -1,1 +1,35 @@
+const mongoose=require('mongoose')
+const Schema=mongoose.Schema;
+const ItemSchema=new Schema({
+    productName:{
+        type:String,
+        required:true
+    },
+    price:{
+        type:Number,
+        required: true,
+    },
+    description:{
+        type:String,
+        required:true
 
+    },
+    image:{
+        type:String,
+        required:true
+    },
+    availableQuantity:{
+        type:Number,
+        required:true
+    },
+    postage:{
+        type:Boolean,
+        required:true
+    }
+
+
+
+
+
+})
+module.exports=mongoose.model('log',ItemSchema)

2590869f42ab71fe109f6fedc9e8efcc2f51a012
Hari Bhandari <2012bhandari.ha@gmail.com>
2020-09-12 12:26:10 +0100

    added login

--- controllers/auth.js
+++ controllers/auth.js
@@ -20,6 +20,28 @@ exports.register=asyncHandler(async (req,res,next)=>{
     sendTokenResponse(user,200,res)
 
 })
+//@desc register a user
+//@route POST /api/v1/auth/login
+//@access Public
+exports.login=asyncHandler(async (req,res,next)=>{
+    const {email,password}=req.body;
+    //Validate email and password
+    if(!email||!password){
+        return next(new ErrorResponse(`Please provide user or a password`,400))
+    }
+    //check for a user
+    const user=await User.findOne({email}).select('+password');
+    if(!user){
+        return next(new ErrorResponse(`Please provide valid user or a password`,401))
+    }
+    //check if password matches
+    const isMatch=await user.matchPassword(password)
+    if(!isMatch){
+        return next(new ErrorResponse(`Please provide valid user or a password`,401))
+    }
+    sendTokenResponse(user,200,res)
+
+})
 //get token from model,create cookie and send
 // response
 const sendTokenResponse=(user,statusCode,res)=>{

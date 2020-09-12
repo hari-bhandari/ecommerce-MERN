9ca6d5aca9c1ec769cf05effc475a9687f786235
Hari Bhandari <2012bhandari.ha@gmail.com>
2020-09-12 14:05:07 +0100

    added add item on controllers/items.js

--- Routes/items.js
+++ Routes/items.js
@@ -1,3 +1,5 @@
 const express=require('express');
 const router=express.Router();
+const {getItem}=require('../controllers/items');
+
 module.exports=router
--- controllers/items.js
+++ controllers/items.js
@@ -2,10 +2,24 @@ const Item=require('../models/Item')
 const asyncHandler=require('../middlewares/async')
 const ErrorResponse=require('../utils/errorResponse')
 const path=require('path')
+
+//@desc Create new  bootcamp
+//@route POST /api/v1/bootcamp:id
+//@access private
+
+exports.addItem=  asyncHandler(async (req,res,next)=>{
+    //add user to req.body
+    req.body.user=req.user.id;
+    const item=await Item.create(req.body)
+    res.status(201).json({
+        success:true,
+        data:item
+    })
+});
 //@desc Get single  item
 //@route GET /api/v1/item:id
 //@access Public
-exports.getBootcamp=asyncHandler(async (req,res,next)=>{
+exports.getItem=asyncHandler(async (req,res,next)=>{
     const bootcamp=await Item.findById(req.params.id)
     if(!bootcamp){
         return next(new ErrorResponse(`Item not found with id of ${req.params.id}`,404))

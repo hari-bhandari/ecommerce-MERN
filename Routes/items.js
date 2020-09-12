ad3df534b663b3aa28449072c5969d285dc5e3a3
Hari Bhandari <2012bhandari.ha@gmail.com>
2020-09-12 21:33:07 +0100

    added get item and get items

--- Routes/items.js
+++ Routes/items.js
@@ -1,9 +1,11 @@
 const express=require('express');
 const router=express.Router();
-const {getItem,addItem,updateItem,deleteItem,itemPhotoUpload}=require('../controllers/items');
+const Item=require('../models/Item')
+const {getItem,getItems,addItem,updateItem,deleteItem,itemPhotoUpload}=require('../controllers/items');
+const advancedResults=require('../middleware/advancedResult')
 
 const {protect,authorize}=require('../middlewares/auth')
-router.route('/').post(protect,authorize('user','admin'),addItem);
-router.route('/:id').put(protect,authorize('user','admin'),updateItem).delete(protect,authorize('user','admin'),deleteItem)
+router.route('/').post(protect,authorize('user','admin'),addItem).get(advancedResults(Item),getItems);
+router.route('/:id').put(protect,authorize('user','admin'),updateItem).delete(protect,authorize('user','admin'),deleteItem).get(getItem)
 router.route('/:id/photo').put(protect,authorize('user','admin'),itemPhotoUpload);
 module.exports=router

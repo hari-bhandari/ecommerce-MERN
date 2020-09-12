3122b23be7d4635c79165d1c835cb56295dd9956
Hari Bhandari <2012bhandari.ha@gmail.com>
2020-09-12 15:06:31 +0100

    added photo upload for item

--- controllers/items.js
+++ controllers/items.js
@@ -69,3 +69,44 @@ exports.deleteItem=asyncHandler(async (req,res,next)=> {
         data: {}
     });
 })
+//@desc upload photo for item
+//@route PUT /api/v1/item/:id/photo
+//@access private
+exports.bootcampPhotoUpload=asyncHandler(async (req,res,next)=> {
+    const itemPhotoUpload=await Item.findById(req.params.id,req.body);
+    const item=await Item.findById(req.params.id);
+
+
+    if(!item){
+        return next(new ErrorResponse(`Bootcamp not found with id of ${req.params.id}`,404))
+    }
+    //make sure user is bootcamp user
+    if(item.user.toString()!==req.user.id&&req.user.role!=='admin'){
+        return next(new ErrorResponse(`User ${req.user.name} is not authorized to update this bootcamp`,401));
+    }
+    if(!req.files){
+        return next(new ErrorResponse(`Please upload a file`,400))
+    }
+    const file=req.files.file;
+    if(!file.mimetype.startsWith('image')){
+        return next(new ErrorResponse(`Please upload an image file`,400))
+    }
+    if(file.size>process.env.MAX_FILE_UPLOAD){
+        return next(new ErrorResponse(`Please upload an image file less than ${process.env.MAX_FILE_UPLOAD}`,400))
+    }
+
+    //create custom file name
+    file.name=`photo_${req.params.id}${path.parse(file.name).ext}`
+    file.mv(`${process.env.FILE_UPLOAD_PATH}/${file.name}`, async err=>{
+        if(err){
+            console.error(err)
+            return next(new ErrorResponse(`problem with file upload `,500))
+        }
+        await item.findByIdAndUpdate(req.params.id,{photo:file.name});
+        res.status(200).json({
+            success:true,
+            data:file.name
+        })
+    })
+    console.log(file.name)
+})

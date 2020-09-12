7ba8adc39221ac7006cabb9364b6e663897c2a58
Hari Bhandari <2012bhandari.ha@gmail.com>
2020-09-12 12:27:45 +0100

    added login on route

--- Routes/auth.js
+++ Routes/auth.js
@@ -1,5 +1,6 @@
 const express=require('express');
-const {register}=require('../controllers/auth');
+const {register,login}=require('../controllers/auth');
 const router=express.Router();
 router.route('/register').post(register);
+router.route('/login').post(login);
 module.exports=router

6774ea1167979cbd2713420002e8db019de1290a
Hari Bhandari <2012bhandari.ha@gmail.com>
2020-09-14 21:30:10 +0100

    added login form

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
-
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Login from "./components/auth/Login";
 function App() {
   return (
     <div className="App">
-
+      <Login/>
     </div>
   );
 }

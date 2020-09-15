1b95fe42448930a6efa2dc1e2b2b56ac47fef27b
Hari Bhandari <2012bhandari.ha@gmail.com>
2020-09-15 12:25:40 +0100

    added navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import './bootstrap.min.css'
+
 import Login from "./components/auth/Login";
 import Signup from "./components/auth/Signup";
 import ShoppingCart from "./components/cart/ShoppingCart";
+import Navbar from "./components/Layout/Navbar";
 function App() {
   return (
     <div className="App">
+        <Navbar/>
       {/*<Login/>*/}
       {/*<Signup/>*/}
       <ShoppingCart/>
---
+++ client/src/components/Layout/Navbar.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import './Navbar.css'
+const Navbar = () => {
+    return (
+        <nav className="navbar navbar-light navbar-expand-md fixed-top bg-light" >
+            <div className="container">
+                <button data-toggle="collapse" className="navbar-toggler" data-target="#navcol-1"><span className="sr-only">Toggle navigation</span><span className="navbar-toggler-icon"></span></button>
+                <div className="collapse navbar-collapse" id="navcol-1">
+                    <ul className="nav navbar-nav flex-grow-1 justify-content-between">
+                        <li className="nav-item"><a className="nav-link" href="#"><i className="fab fa-apple apple-logo"></i></a></li>
+                        <li className="nav-item"><a className="nav-link" href="#">Iphone</a></li>
+                        <li className="nav-item"><a className="nav-link" href="#">Samsung</a></li>
+                        <li className="nav-item"><a className="nav-link" href="#">Huawei</a></li>
+                        <li className="nav-item"><a className="nav-link" href="#">LG</a></li>
+                        <li className="nav-item"><a className="nav-link" href="#">TV</a></li>
+                        <li className="nav-item"><a className="nav-link" href="#">Support</a></li>
+                        <li className="nav-item"><a className="nav-link" href="#"><i className="fa fa-shopping-bag"></i></a></li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    );
+};
+
+export default Navbar;

6827e2a3d4c6d75b0753c373c6b076aee85c33eb
Hari Bhandari <2012bhandari.ha@gmail.com>
2020-09-15 15:39:45 +0100

    added getUserLoggedIn

--- client/src/context/auth/AuthState.js
+++ client/src/context/auth/AuthState.js
@@ -3,6 +3,9 @@ import React, { useReducer } from 'react';
 import axios from 'axios';
 import AuthContext from './authContext';
 import authReducer from './authReducer';
+import {
+    GET_TOKEN, REMOVE_TOKEN
+} from '../types'
 const AuthState=props=>{
     const initialState = {
         token: '',
@@ -11,6 +14,31 @@ const AuthState=props=>{
         user: null,
         error: null
     };
+    //get user logged in using
+    const getUserLoggedIn=async (formData)=>{
+        const config = {
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        };
+        try {
+            const res = await axios.post('/api/auth/login', formData, config);
+
+            dispatch({
+                type: GET_TOKEN,
+                payload: res.data.token
+            });
+
+
+        } catch (err) {
+            dispatch({
+                type: REMOVE_TOKEN,
+                payload: err.response.data.error
+            })
+        }
+
+
+    }
     const[state,dispatch]=useReducer(authReducer,initialState)
     return(
         <AuthContext.Provider value={{
@@ -18,7 +46,8 @@ const AuthState=props=>{
             isAuthenticated:state.isAuthenticated,
             loading:state.loading,
             user:state.user,
-            error:state.error
+            error:state.error,
+            getUserLoggedIn
         }}>{props.children}</AuthContext.Provider>
     )
 

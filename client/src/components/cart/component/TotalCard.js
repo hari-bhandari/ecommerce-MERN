1b29d213cf8f055155c753d45503034d9ba25429
Hari Bhandari <2012bhandari.ha@gmail.com>
2020-09-15 10:56:49 +0100

    destructured pay npow to new card

---
+++ client/src/components/cart/component/TotalCard.js
@@ -0,0 +1,35 @@
+import React from 'react';
+
+const TotalCard = () => {
+    return (
+        <div className="summary">
+            <div className="summary-total-items"><span className="total-items"></span> Items in your Bag</div>
+            <div className="summary-subtotal">
+                <div className="subtotal-title">Subtotal</div>
+                <div className="subtotal-value final-value" id="basket-subtotal">130.00</div>
+                <div className="summary-promo hide">
+                    <div className="promo-title">Promotion</div>
+                    <div className="promo-value final-value" id="basket-promo"></div>
+                </div>
+            </div>
+            <div className="summary-delivery">
+                <select name="delivery-collection" className="summary-delivery-selection">
+                    <option value="0" selected="selected">Select Collection or Delivery</option>
+                    <option value="collection">Collection</option>
+                    <option value="first-class">Royal Mail 1st Class</option>
+                    <option value="second-class">Royal Mail 2nd Class</option>
+                    <option value="signed-for">Royal Mail Special Delivery</option>
+                </select>
+            </div>
+            <div className="summary-total">
+                <div className="total-title">Total</div>
+                <div className="total-value final-value" id="basket-total">130.00</div>
+            </div>
+            <div className="summary-checkout">
+                <button className="checkout-cta">Go to Secure Checkout</button>
+            </div>
+        </div>
+    );
+};
+
+export default TotalCard;

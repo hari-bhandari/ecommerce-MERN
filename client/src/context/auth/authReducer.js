a02ac3d66f762f8e7fe69afe7d412688e18acb2f
Hari Bhandari <2012bhandari.ha@gmail.com>
2020-09-15 16:08:43 +0100

    added reducer for get TOKEN and REMOVE TOKEN

--- client/src/context/auth/authReducer.js
+++ client/src/context/auth/authReducer.js
@@ -1,10 +1,28 @@
 
 import {
-
+GET_TOKEN,REMOVE_TOKEN
 } from '../types'
 export default (state,action)=>{
     switch(action.type){
-        case CLEAR_ERRORS:
+        case GET_TOKEN:
+            localStorage.setItem('token',action.payload)
+            return{
+                ...state,
+                token:action.payload,
+                isAuthenticated:true,
+                loading:false
+            }
+        case REMOVE_TOKEN:
+            localStorage.removeItem('token')
+            return{
+                ...state,
+                token:null,
+                isAuthenticated: false,
+                loading: false,
+                user:null,
+                error:action.payload
+
+            }
             return {
                 ...state,
                 error: null

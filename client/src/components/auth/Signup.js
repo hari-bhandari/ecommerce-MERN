118ab088dc8142508d0da9fd31d3cbd194673af7
Hari Bhandari <2012bhandari.ha@gmail.com>
2020-09-16 11:09:33 +0100

    added state for signup form

--- client/src/components/auth/Signup.js
+++ client/src/components/auth/Signup.js
@@ -1,8 +1,16 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './login.css'
 import signup from './img/signup.svg'
 
 const Signup = () => {
+    const [user,setUser]=useState({
+        name:'',
+        email:'',
+        password:'',
+        password2:''})
+    const onChange=(e)=>{
+        setUser({...user,[e.target.name]:e.target.value})
+    }
     return (
         <div className="limiter">
             <div className="container-login100">
@@ -15,28 +23,28 @@ const Signup = () => {
 						Sign up to access amazing deals
 					</span>
                         <div className="wrap-input100 validate-input">
-                            <input className="input100" type="text" name="name" placeholder="Name"/>
+                            <input className="input100" type="text" name="name" placeholder="Name" onChange={onChange}/>
                             <span className="focus-input100"></span>
                             <span className="symbol-input100">
 							<i className="fa fa-user" aria-hidden="true"></i>
 						</span>
                         </div>
                         <div className="wrap-input100 validate-input" data-validate="Valid email is required: ex@abc.xyz">
-                            <input className="input100" type="email" name="email" placeholder="Email"/>
+                            <input className="input100" type="email" name="email" placeholder="Email"  onChange={onChange}/>
                             <span className="focus-input100"></span>
                             <span className="symbol-input100">
 							<i className="fa fa-envelope" aria-hidden="true"></i>
 						</span>
                         </div>
                         <div className="wrap-input100 validate-input" data-validate="Password is required">
-                            <input className="input100" type="password" name="pass" placeholder="Password"/>
+                            <input className="input100" type="password" name="password" placeholder="Password"  onChange={onChange}/>
                             <span className="focus-input100"></span>
                             <span className="symbol-input100">
 							<i className="fa fa-lock" aria-hidden="true"></i>
 						</span>
                         </div>
                         <div className="wrap-input100 validate-input" data-validate="Password is required">
-                            <input className="input100" type="password" name="pass" placeholder=" Confirm Password"/>
+                            <input className="input100" type="password" name="password2" placeholder=" Confirm Password"  onChange={onChange}/>
                             <span className="focus-input100"></span>
                             <span className="symbol-input100">
 							<i className="fa fa-lock" aria-hidden="true"></i>

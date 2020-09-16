0e6fc652f7b08c9b12f2f8dc90938ed8a387b883
Hari Bhandari <2012bhandari.ha@gmail.com>
2020-09-16 09:58:57 +0100

    added getme() on route

--- Routes/auth.js
+++ Routes/auth.js
@@ -1,6 +1,8 @@
 const express=require('express');
-const {register,login}=require('../controllers/auth');
+const {protect}=require('../middlewares/auth')
+const {register,login,getMe}=require('../controllers/auth');
 const router=express.Router();
 router.route('/register').post(register);
 router.route('/login').post(login);
+router.route('/me').get(protect,getMe)
 module.exports=router

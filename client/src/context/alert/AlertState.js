eae0e473ecd93acb4ea1316acf605266ae59fb90
Hari Bhandari <2012bhandari.ha@gmail.com>
2020-09-17 19:46:14 +0100

    added set_alert method

--- client/src/context/alert/AlertState.js
+++ client/src/context/alert/AlertState.js
@@ -1,15 +1,24 @@
 import React, {useReducer} from 'react';
 import AlertContext from './alertContext';
 import alertReducer from './alertReducer';
-
+import {SET_ALERT, REMOVE_ALERT} from '../types';
 const AlertState = props => {
     const initialState = [];
 
     const [state, dispatch] = useReducer(alertReducer, initialState);
+    const setAlert = (msg, type, timeout = 5000) => {
+        const id = uuidv4();
+        dispatch({
+            type: SET_ALERT,
+            payload: {msg, type, id},
+        });
+
+    };
     return (
         <AlertContext.Provider
             value={{
                 alerts: state,
+                setAlert
             }}>
             {props.children}
         </AlertContext.Provider>

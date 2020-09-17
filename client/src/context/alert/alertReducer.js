ac3f0037d044e4147820fe9a9f5aeaa333cde9a2
Hari Bhandari <2012bhandari.ha@gmail.com>
2020-09-17 19:47:01 +0100

    added set_ALERT reducer

--- client/src/context/alert/alertReducer.js
+++ client/src/context/alert/alertReducer.js
@@ -1,7 +1,8 @@
-
+import { SET_ALERT, REMOVE_ALERT } from '../types';
 export default (state, action) => {
     switch (action.type) {
-
+        case SET_ALERT:
+            return [...state, action.payload];
         default:
             return state;
     }

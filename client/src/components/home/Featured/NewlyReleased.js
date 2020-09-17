c7ba9677d79f98ea0f8226267a70c2990d02891c
Hari Bhandari <2012bhandari.ha@gmail.com>
2020-09-17 09:18:20 +0100

    added layout for newly released section

---
+++ client/src/components/home/Featured/NewlyReleased.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const NewlyReleased = () => {
+    return (
+        <div className="container">
+            <h2>Newly Released</h2>
+            <div className="row">
+
+            </div>
+
+        </div>
+    );
+};
+
+export default NewlyReleased;

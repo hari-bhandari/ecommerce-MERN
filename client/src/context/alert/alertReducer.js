d3234cf75d0aa2b1902703a7f3fdbcc7fc5cf6c4
Hari Bhandari <2012bhandari.ha@gmail.com>
2020-09-17 19:48:24 +0100

    added reducer for REMOVE alert

--- client/src/context/alert/alertReducer.js
+++ client/src/context/alert/alertReducer.js
@@ -3,6 +3,8 @@ export default (state, action) => {
     switch (action.type) {
         case SET_ALERT:
             return [...state, action.payload];
+        case REMOVE_ALERT:
+            return state.filter(alert => alert.id !== action.payload);
         default:
             return state;
     }

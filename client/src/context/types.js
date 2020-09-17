e4d538d7ae1a6c0d149edcfdee8ed4402efb9d56
Hari Bhandari <2012bhandari.ha@gmail.com>
2020-09-17 19:45:19 +0100

    added SET_ALERT and remove_ALERT on types and exported it

--- client/src/context/types.js
+++ client/src/context/types.js
@@ -3,3 +3,8 @@ export const REMOVE_TOKEN='REMOVE_TOKEN';
 export const LOAD_USER='LOAD_USER';
 export const LOAD_USER_FAIL='LOAD_USER_FAIL';
 export const CLEAR_ERROR='CLEAR_ERROR';
+
+
+
+export const SET_ALERT = 'SET_ALERT';
+export const REMOVE_ALERT = 'REMOVE_ALERT';

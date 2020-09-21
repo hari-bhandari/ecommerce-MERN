6e57d69eb05d707ca59847e0b6ba90e555c4fcfc
Hari Bhandari <2012bhandari.ha@gmail.com>
2020-09-21 06:44:59 +0100

    few changes

--- client/src/context/items/itemReducer.js
+++ client/src/context/items/itemReducer.js
@@ -1,6 +1,14 @@
+import {GET_ITEMS_QUERY} from "../types";
+
 export default (state, action) => {
     switch (action.type) {
+        case GET_ITEMS_QUERY:
+            return {
+                ...state,
+                items:action.payload,
+                loading:false
 
+            }
         default:
             return state;
     }

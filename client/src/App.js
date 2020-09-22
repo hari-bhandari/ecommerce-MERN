114835b27080f241400e2a881fc83d9f6c2aa8cf
Hari Bhandari <2012bhandari.ha@gmail.com>
2020-09-22 06:38:31 +0100

    removed unused imports

--- client/src/App.js
+++ client/src/App.js
@@ -3,11 +3,10 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Login from "./components/auth/Login";
-// import ShoppingCart from "./components/cart/ShoppingCart";
 import Navbar from "./components/Layout/Navbar";
+//importing state contexts
 import AuthState from './context/auth/AuthState'
-// import NewlyReleased from "./components/home/Featured/NewlyReleased";
-// import FeaturedPage from "./components/home/Featured/FeaturedPage";
+
 import HomePage from "./components/pages/HomePage";
 import Signup from "./components/auth/Signup";
 

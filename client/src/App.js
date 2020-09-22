c9a598a1463a8f3ccb26ba37fc9c2a549f8747e3
Hari Bhandari <2012bhandari.ha@gmail.com>
2020-09-22 06:44:01 +0100

    implemented item state

--- client/src/App.js
+++ client/src/App.js
@@ -1,29 +1,34 @@
 import React, {Fragment} from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css'
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import Login from "./components/auth/Login";
 import Navbar from "./components/Layout/Navbar";
+import HomePage from "./components/pages/HomePage";
+import Signup from "./components/auth/Signup";
+
 //importing state contexts
 import AuthState from './context/auth/AuthState'
 import AlertState from "./context/alert/AlertState";
-import HomePage from "./components/pages/HomePage";
-import Signup from "./components/auth/Signup";
+import ItemState from "./context/items/ItemState";
+
 
 function App() {
     return (
         <AuthState>
             <AlertState>
-            <Router>
-                <Fragment>
-                    <Navbar />
+                <ItemState/>
+                <Router>
+                    <Fragment>
+                        <Navbar/>
                         <Switch>
-                            <Route exact path='/' component={HomePage} />
-                            <Route exact path='/signup' component={Signup} />
-                            <Route exact path='/login' component={Login} />
+                            <Route exact path='/' component={HomePage}/>
+                            <Route exact path='/signup' component={Signup}/>
+                            <Route exact path='/login' component={Login}/>
                         </Switch>
-                </Fragment>
-            </Router>
+                    </Fragment>
+                </Router>
+                <ItemState/>
             </AlertState>
         </AuthState>
   );

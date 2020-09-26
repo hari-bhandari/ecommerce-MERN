2fc17e80ca62ca4f08131f5e25763b2e2d5331d1
Hari Bhandari <2012bhandari.ha@gmail.com>
2020-09-26 13:44:35 +0100

    loaduser imlemented on homepage useeffect

--- client/src/components/pages/HomePage.js
+++ client/src/components/pages/HomePage.js
@@ -2,14 +2,21 @@ import React, {useContext, useEffect} from 'react';
 import FeaturedPage from "../home/Featured/FeaturedPage";
 import NewlyReleased from "../home/Featured/NewlyReleased";
 import ItemContext from "../../context/items/itemContext";
+import AuthContext from "../../context/auth/authContext";
 const HomePage = () => {
     const itemContext=useContext(ItemContext)
     const{apple,getItemsByQuery,samsung,watch}=itemContext
 
+    const authContext=useContext(AuthContext);
+    const {loadUser}=authContext;
+
+
     useEffect(()=>{
+
         getItemsByQuery('apple')
         getItemsByQuery('samsung')
         getItemsByQuery('watch')
+        loadUser()
     },[])
     return (
         <div style={{marginTop:'4em'}}>

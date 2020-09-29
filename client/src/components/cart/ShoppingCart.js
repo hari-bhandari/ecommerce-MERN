1cb22ff865c793e18fd0cf8660d035ab027e8dc0
Hari Bhandari <2012bhandari.ha@gmail.com>
2020-09-29 20:25:13 +0100

    implemented auth state and mapped cart

--- client/src/components/cart/ShoppingCart.js
+++ client/src/components/cart/ShoppingCart.js
@@ -1,17 +1,23 @@
-import React from 'react';
+import React, {useContext, useEffect} from 'react';
 import './shoppingCart.css'
 import CartItem from "./component/CartItem";
+import AuthContext from "../../context/auth/authContext";
 
 const ShoppingCart = () => {
+    const authContext=useContext(AuthContext);
+    const {getCart,isAuthenticated,cart,loadUser}=authContext;
+    useEffect(()=>{
+        loadUser()
+        if(isAuthenticated){
+            getCart()
+        }
+    },[isAuthenticated])
     return (
         <section className="my-cart p-0 mt-8">
-
-
             <div className="container py-5">
-                <CartItem/>
-                <CartItem/>
-                <CartItem/>
-                <CartItem/>
+                {cart&&cart.map(item=>(
+                    <CartItem/>
+                ))}
 
             </div>
             <button className="btn btn-block bg-success float-right" style={{width:'300px'}}>Proceed to checkout</button>

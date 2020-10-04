d8aed9bb95cdb872789521e835aff68a5a08c594
Hari Bhandari <2012bhandari.ha@gmail.com>
2020-10-04 13:45:47 +0100

    added 404

--- client/src/components/pages/CategoryPage.js
+++ client/src/components/pages/CategoryPage.js
@@ -1,15 +1,28 @@
-import React, {useContext, useEffect, useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import NewlyReleased from "../home/Featured/NewlyReleased";
-import ItemContext from "../../context/items/itemContext";
-const CategoryPage = ({match,history}) => {
-    const itemContext=useContext(ItemContext)
-    const{apple,getItemsByQuery,samsung,watch}=itemContext
+import axios from "axios";
+import { useHistory } from "react-router-dom";
+const CategoryPage = ({match}) => {
+    const history=useHistory()
+    const [data, setData] = useState(null)
+    const getItemsByQuery = async (query) => {
+        try {
+            const res = await axios.get(`/api/items/?category=${query}&limit=12`);
+            setData(res.data.data)
+        } catch (err) {
+            setData('Error')
+        }
+    }
 
     useEffect(() => {
         getItemsByQuery(match.params.itemSlug);
         // eslint-disable-next-line
     }, []);
-
+    if (data) {
+        if (data == 'Error' || data.length == 0) {
+            return (<div>Are you Lost?</div>)
+        }
+    }
     return (
         <div>
             <NewlyReleased data={data}/>

dd9568b4fc1c9e957e2301a6712e83ef978b8d29
Hari Bhandari <2012bhandari.ha@gmail.com>
2020-10-04 10:15:13 +0100

    added get Item b y query on catogory page

--- client/src/components/pages/CategoryPage.js
+++ client/src/components/pages/CategoryPage.js
@@ -1,10 +1,27 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import NewlyReleased from "../home/Featured/NewlyReleased";
+import axios from "axios";
 
-const CategoryPage = () => {
+const CategoryPage = ({match}) => {
+    const[data,setData]=useState(null)
+    const getItemsByQuery=async (query)=>{
+        try {
+            const res = await axios.get(`/api/items/?category=${query}&limit=4`);
+            setData(res.data.data)
+        } catch (err) {
+            setData(null)
+        }
+    }
+
+    useEffect(() => {
+
+        getItemsByQuery(match.params.login);
+        // eslint-disable-next-line
+    }, []);
+
     return (
         <div>
-            <NewlyReleased/>
+            <NewlyReleased data={data}/>
         </div>
     );
 };

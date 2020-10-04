5ef5d1cbc67848caaafe3df3e35cbbe3a31724f5
Hari Bhandari <2012bhandari.ha@gmail.com>
2020-10-04 15:31:15 +0100

    made react  build static

--- server.js
+++ server.js
@@ -39,9 +39,13 @@ app.use(express.static(path.join(__dirname,'public')))
 
 const server=app.listen(PORT,()=>console.log(`server running in Production mode on port ${PORT}`))
 
+app.use(express.static('client/build'));
 
+app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+);
 //handle unhandled promised rejections
 process.on('unhandledRejection',(err,promise)=>{
-    // console.log(`error:${err.message}`)
+    console.log(`error:${err.message}`)
     server.close(()=>process.exit(1))
 });

5e6a7fd3d4f3a52000740ee6e436f849a9913e02
hari-bhandari <2012bhandari.ha@gmail.com>
2021-02-17 22:33:34 +0000

    made it only show on larger screens

--- client-new/.next/static/chunks/webpack.js
+++ client-new/.next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "5682efec62d5aebd1802";
+/******/ 	var hotCurrentHash = "fa8617baaec5f138cdcb";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;

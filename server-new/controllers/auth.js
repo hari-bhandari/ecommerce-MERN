cc7a72e03795e185a54ab32aeb00001e23344265
hari-bhandari <2012bhandari.ha@gmail.com>
2021-03-24 23:18:14 +0000

    added forgot password

--- server-new/controllers/auth.js
+++ server-new/controllers/auth.js
@@ -3,6 +3,7 @@ const ErrorResponse = require('../utils/errorResponse');
 const asyncHandler = require('../middleware/async');
 const User = require('../models/user');
 const mailchimp = require('../services/mailchimp');
+const mailgun = require("../config/mailgun");
 
 // @desc      Get current logged in user
 // @route     POST /api/v1/auth/me
@@ -46,36 +47,14 @@ exports.forgotPassword = asyncHandler(async (req, res, next) => {
 
     await user.save({ validateBeforeSave: false });
 
-    // Create reset url
-    const resetUrl = `${req.protocol}://${req.get(
-        'host'
-    )}/api/v1/auth/resetpassword/${resetToken}`;
-
-    const message = `You are receiving this email because you (or someone else) has requested the reset of a password. Please make a PUT request to: \n\n ${resetUrl}`;
-
-    try {
-        await sendEmail({
-            email: user.email,
-            subject: 'Password reset token',
-            message,
-        });
-
-        return res.status(200).json({
-            success: true,
-            data: `Email sent to ${user.email}`,
-        });
-    } catch (err) {
-        console.log(err);
-        user.resetPasswordToken = undefined;
-        user.resetPasswordExpire = undefined;
-
-        await user.save({
-            validateBeforeSave: false,
-        });
-
-        return next(new ErrorResponse('Email could not be send', 500));
+    await mailgun.sendEmail(
+        req.body.email,
+        'reset',
+        req.headers.host,
+        resetToken
+    );
     }
-});
+);
 
 // @desc      Update user details
 // @route     PUT /api/v1/auth/updatedetails
--- server-new/routes/api/auth.js
+++ server-new/routes/api/auth.js
@@ -4,7 +4,7 @@ const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const crypto = require('crypto');
 const passport = require('passport');
-const {login} =require('../../controllers/auth')
+const {login,register,forgotPassword} =require('../../controllers/auth')
 const auth = require('../../middleware/auth');
 
 // Bring in Models & Helpers
@@ -17,144 +17,9 @@ const { secret, tokenLife } = keys.jwt;
 
 router.post('/login', login)
 
-router.post('/register', (req, res) => {
-  const email = req.body.email;
-  const firstName = req.body.firstName;
-  const lastName = req.body.lastName;
-  const password = req.body.password;
-  const isSubscribed = req.body.isSubscribed;
+router.post('/register',register)
 
-  if (!email) {
-    return res.status(400).json({ error: 'You must enter an email address.' });
-  }
-
-  if (!firstName || !lastName) {
-    return res.status(400).json({ error: 'You must enter your full name.' });
-  }
-
-  if (!password) {
-    return res.status(400).json({ error: 'You must enter a password.' });
-  }
-
-  User.findOne({ email }, async (err, existingUser) => {
-    if (err) {
-      next(err);
-    }
-
-    if (existingUser) {
-      return res
-        .status(400)
-        .json({ error: 'That email address is already in use.' });
-    }
-
-    let subscribed = false;
-    if (isSubscribed) {
-      const result = await mailchimp.subscribeToNewsletter(email);
-
-      if (result.status === 'subscribed') {
-        subscribed = true;
-      }
-    }
-
-    const user = new User({
-      email,
-      password,
-      firstName,
-      lastName
-    });
-
-    bcrypt.genSalt(10, (err, salt) => {
-      bcrypt.hash(user.password, salt, (err, hash) => {
-        if (err) {
-          return res.status(400).json({
-            error: 'Your request could not be processed. Please try again.'
-          });
-        }
-
-        user.password = hash;
-
-        user.save(async (err, user) => {
-          if (err) {
-            return res.status(400).json({
-              error: 'Your request could not be processed. Please try again.'
-            });
-          }
-
-          const payload = {
-            id: user.id
-          };
-
-          await mailgun.sendEmail(user.email, 'signup', null, user.profile);
-
-          jwt.sign(payload, secret, { expiresIn: tokenLife }, (err, token) => {
-            res.status(200).json({
-              success: true,
-              subscribed,
-              token: `Bearer ${token}`,
-              user: {
-                id: user.id,
-                firstName: user.firstName,
-                lastName: user.lastName,
-                email: user.email,
-                role: user.role
-              }
-            });
-          });
-        });
-      });
-    });
-  });
-});
-
-router.post('/forgot', (req, res) => {
-  const email = req.body.email;
-
-  if (!email) {
-    return res.status(400).json({ error: 'You must enter an email address.' });
-  }
-
-  User.findOne({ email }, (err, existingUser) => {
-    if (err || existingUser === null) {
-      return res.status(400).json({
-        error:
-          'Your request could not be processed as entered. Please try again.'
-      });
-    }
-
-    crypto.randomBytes(48, (err, buffer) => {
-      const resetToken = buffer.toString('hex');
-      if (err) {
-        return res.status(400).json({
-          error: 'Your request could not be processed. Please try again.'
-        });
-      }
-
-      existingUser.resetPasswordToken = resetToken;
-      existingUser.resetPasswordExpires = Date.now() + 3600000;
-
-      existingUser.save(async err => {
-        if (err) {
-          return res.status(400).json({
-            error: 'Your request could not be processed. Please try again.'
-          });
-        }
-
-        await mailgun.sendEmail(
-          existingUser.email,
-          'reset',
-          req.headers.host,
-          resetToken
-        );
-
-        res.status(200).json({
-          success: true,
-          message:
-            'Please check your email for the link to reset your password.'
-        });
-      });
-    });
-  });
-});
+router.post('/forgotpassword', forgotPassword)
 
 router.post('/reset/:token', (req, res) => {
   const password = req.body.password;

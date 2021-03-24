b20e953c899fc17af70e68b42d4277cb813fb720
hari-bhandari <2012bhandari.ha@gmail.com>
2021-03-24 22:41:10 +0000

    new server up and running

---
+++ server-new/server.js
@@ -0,0 +1,41 @@
+require('dotenv').config();
+const express = require('express');
+const chalk = require('chalk');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const passport = require('passport');
+
+const keys = require('./config/keys');
+const routes = require('./routes');
+
+const { database, port } = keys;
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(cors());
+app.use(passport.initialize());
+
+// Connect to MongoDB
+mongoose.set('useCreateIndex', true);
+mongoose
+  .connect(database.url, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false
+  })
+  .then(() =>
+    console.log(`${chalk.green('✓')} ${chalk.blue('MongoDB Connected!')}`)
+  )
+  .catch(err => console.log(err));
+
+require('./config/passport');
+app.use(routes);
+
+app.listen(port, () => {
+  console.log(
+    `${chalk.green('✓')} ${chalk.blue(
+      `Listening on port ${port}. Visit http://localhost:${port}/ in your browser.`
+    )}`
+  );
+});

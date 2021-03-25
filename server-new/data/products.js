c7c3d5598a5ed011d641c2dcc570bfe8e497a34a
hari-bhandari <2012bhandari.ha@gmail.com>
2021-03-25 23:15:10 +0000

    added seeder and some data as well

---
+++ server-new/data/products.js
@@ -0,0 +1,101 @@
+const products = [
+  {
+    name: "Airpods Wireless Bluetooth Headphones",
+    thumbImage: "https://lookagain.scene7.com/is/image/OttoUK/553w/airpods-pro-by-apple~39X306FRSP.jpg",
+    description:
+        "Bluetooth technology lets you connect it with compatible devices wirelessly High-quality AAC audio offers immersive listening experience Built-in microphone allows you to take calls while working",
+    subCategory: "apple",
+    category: "Earphones",
+    price: 89.99,
+    countInStock: 3,
+    rating: 0,
+    numReviews: 0,
+    id:'airpods-wireless'
+  },
+  {
+    name: "iPhone 11 Pro 256GB Memory",
+    thumbImage: "https://www.pricerunner.com/product/200x200/1906996022/Apple-iPhone-11-64GB.jpg",
+    description:
+        "Introducing the iPhone 11 Pro. A transformative triple-camera system that adds tons of capability without complexity. An unprecedented leap in battery life",
+    subCategory: "apple",
+    category: "Smart phones",
+    price: 599.99,
+    countInStock: 10,
+    rating: 0,
+    numReviews: 0,
+    id:'iphone-11'
+
+  },
+  {
+    name: "Cannon EOS 80D DSLR Camera",
+    thumbImage: "https://www.wilkinson.co.uk/media/catalog/product/cache/1/image/9df78eab33525d08d6e5fb8d27136e95/n/i/nikon_d3500_dslr_camera_af-p_dx_18-55mm_vr_lens_007.jpg",
+    description:
+        "Characterized by versatile imaging specs, the Canon EOS 80D further clarifies itself using a pair of robust focusing systems and an intuitive design",
+    subCategory: "Cannon",
+    category: "Camera",
+    price: 929.99,
+    countInStock: 0,
+    rating: 0,
+    numReviews: 0,
+    id:'cannon-eos'
+  },
+  {
+    name: "Sony Playstation 4 Pro White Version",
+    thumbImage: "https://gmedia.playstation.com/is/image/SIEPDC/ps4-slim-image-block-01-en-24jul20?$native--t$",
+    description:
+        "The ultimate home entertainment center starts with PlayStation. Whether you are into gaming, HD movies, television, music",
+    subCategory: "Sony",
+    category: "playstation",
+    price: 399.99,
+    countInStock: 10,
+    rating: 0,
+    numReviews: 0,
+    id:'sony-ps4'
+
+  },
+  {
+    name: "Logitech G-Series Gaming Mouse",
+    thumbImage: "https://www.memoryc.com/images/products/bb/vmmm_31692.jpg",
+    description:
+        "Get a better handle on your games with this Logitech LIGHTSYNC gaming mouse. The six programmable buttons allow customization for a smooth playing experience",
+    subCategory: "Logitech",
+    category: "Mouse",
+    price: 49.99,
+    countInStock: 7,
+    rating: 0,
+    numReviews: 0,
+    id:'logitech-mouse'
+
+  },
+  {
+    name: "Amazon Echo Dot 3rd Generation",
+    thumbImage: "https://www.scan.co.uk/images/products/3032497-a.jpg",
+    description:
+        "Meet Echo Dot - Our most popular smart speaker with a fabric design. It is our most compact smart speaker that fits perfectly into small space",
+    subCategory: "Amazon",
+    category: "smart speakers",
+    price: 29.99,
+    countInStock: 12,
+    rating: 5,
+    numReviews: 23,
+    id:'amazonEcho'
+
+  },
+  {
+    name: "Iphone 6",
+    thumbImage: "https://recosi.net/wp-content/uploads/2017/11/Iphone-6-Silver-1.jpg",
+    description:
+        "The iPhone is a smartphone made by Apple that combines a computer, iPod, digital camera and cellular phone into one device with a touchscreen interface. The iPhone runs the iOS operating system (OS), and as of 2017, there were 2.2 million apps available for it through the Apple App Store, according to Statista.",
+    subCategory: "iphone",
+    category: "phones",
+    price: 29.99,
+    countInStock: 12,
+    rating: 5,
+    numReviews: 23,
+    id:'iphone-6'
+
+  },
+
+]
+
+export default products

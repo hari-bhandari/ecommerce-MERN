2f37f4ddd734071417222b0979f3833e47d6025c
hari-bhandari <2012bhandari.ha@gmail.com>
2021-04-02 00:14:11 +0100

    added get all subcategories for a category

--- server-new/routes/api/subCategory.js
+++ server-new/routes/api/subCategory.js
@@ -14,12 +14,16 @@ router.post('/:id', auth, role.checkRole(role.ROLES.Admin), asyncHandler(async (
     const {name,id} = req.body
     const category = await Category.findById(req.params.id)
     if (category) {
-        const subCategory=await SubCategory.create({name,id,category:req.params.id})
+        const subCategory=await SubCategory.create({name,id,category:category._id})
         res.status(201).json({data:subCategory})
     } else {
         res.status(404)
         throw new Error('Category  not found with an ID of ' +req.params.id)
     }
+}))
+router.get('/:id',asyncHandler(async (req, res) => {
+    const subCategories=await SubCategory.find({category:req.params.id})
+    res.status(201).json({data:subCategories})
 }))
 
 

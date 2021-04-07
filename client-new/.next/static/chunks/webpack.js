3df1dac05d004084322b54c2b67bbf3130aefbbe
hari-bhandari <2012bhandari.ha@gmail.com>
2021-04-07 22:10:08 +0100

    added script to run admin and client individually

--- client-new/.next/static/chunks/webpack.js
+++ client-new/.next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "8ceaca2843659a556aa6";
+/******/ 	var hotCurrentHash = "9dee33253d52a53c393b";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;

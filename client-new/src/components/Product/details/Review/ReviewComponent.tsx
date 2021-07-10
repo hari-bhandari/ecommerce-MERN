7dfc31196be46f40af91c0fe41e704520fa9e892
Hari Bhandari <2012bhandari.ha@gmail.com>
2021-07-10 21:20:38 +0100

    reviews working fine noew. just need to integrate api

--- client-new/src/components/Product/details/Review/ReviewComponent.tsx
+++ client-new/src/components/Product/details/Review/ReviewComponent.tsx
@@ -3,17 +3,20 @@ import {AddReviewContainer, ReviewsContainer} from './ReviewComponent.style';
 
 import ReviewIcon from "@/assets/icons/ReviewIcon";
 import Reviews from "@/components/Product/details/Review/Reviews";
+import {ReviewTitle} from "@/components/Product/details/ProductDetails.style";
 
 const ReviewComponent = () => {
     return (
+        <>
+            <ReviewTitle>Reviews</ReviewTitle>
         <ReviewsContainer>
-
             <AddReviewContainer>
                 <ReviewIcon width={"75px"} height={"75px"}/>
                 <span>Add a review</span>
             </AddReviewContainer>
             <Reviews/>
         </ReviewsContainer>
+            </>
     );
 };
 

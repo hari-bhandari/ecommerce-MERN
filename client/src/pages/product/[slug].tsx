afdfa02d907bbc19e67139600b2df6cdb7b0474a
Hari Bhandari <2012bhandari.ha@gmail.com>
2021-07-11 16:49:39 +0100

    removed and added required dependency

--- client/src/pages/product/[slug].tsx
+++ client/src/pages/product/[slug].tsx
@@ -7,7 +7,6 @@ import ProductSingleWrapper, {
     ProductSingleContainer,
 } from '../../../styles/product-style';
 import {API_BASE_URL} from "@/utils/config";
-import Layout from "../../components/Layout/layout";
 import ProductNotFound from "@/components/404/ProductNotFound";
 import ProductDetails from "@/components/Product/details/ProductDetails";
 import axios from "axios";

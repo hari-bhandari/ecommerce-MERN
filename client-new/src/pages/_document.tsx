05926be8cb57dfcecc75c6806f0592365018bda6
Hari Bhandari <2012bhandari.ha@gmail.com>
2021-07-11 14:14:01 +0100

    basicx

--- client-new/src/pages/_document.tsx
+++ client-new/src/pages/_document.tsx
@@ -6,7 +6,6 @@ import Document, {
     DocumentContext,
 } from "next/document";
 import { ServerStyleSheet } from "styled-components";
-
 export default class CustomDocument extends Document {
     static async getInitialProps(ctx: DocumentContext) {
         const sheet = new ServerStyleSheet();

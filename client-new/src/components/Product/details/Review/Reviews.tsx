a0a590563a7b036c780f93e00048c31d40ccf6cf
Hari Bhandari <2012bhandari.ha@gmail.com>
2021-07-11 14:14:01 +0100

    removed .next files

--- client-new/src/components/Product/details/Review/Reviews.tsx
+++ client-new/src/components/Product/details/Review/Reviews.tsx
@@ -4,7 +4,9 @@ import StarRating from "@/components/Product/StarRating";
 import {ReviewTitle} from "@/components/Product/details/ProductDetails.style";
 import {TagsContainer, TagsHeader, TagsWrapper} from "@/components/Product/details/Review/ReviewComponent.style";
 import {Scrollbar} from "@/components/Scrollbar";
-const ReviewCardContainer=styled.div`
+import {Button} from "@/components/Others/button/button";
+
+const ReviewCardContainer = styled.div`
   display: flex;
   flex-direction: column;
   justify-content: space-around;
@@ -16,45 +18,51 @@ const ReviewCardContainer=styled.div`
   margin: 0.5px 0px;
   box-shadow: #eeeeee 2px 2px 2px 2px;
 `
-const ReviewHeader=styled.div`
+const ReviewHeader = styled.div`
   display: flex;
   align-items: center;
   justify-content: space-around;
   color: #000000;
   font-weight: bold;`
-const NameGroup=styled.div`
+const NameGroup = styled.div`
   display: flex;
   align-items: center;
 `
-const Initials=styled.div`
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    width: 35px;
-    height: 35px;
-    margin-right: 12px;
-    border-radius: 50%;
-    background: #d56a6a;
+const Initials = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 35px;
+  height: 35px;
+  margin-right: 12px;
+  border-radius: 50%;
+  background: #d56a6a;
 `
-const Description=styled.div`
+const Description = styled.div`
   color: #2b2b2b;
   font-weight: 400;
   margin: 0px 45px;
 `
-const Details=styled.div`
+const Details = styled.div`
   display: flex;
   justify-content: space-between;
   margin: 0px 45px;
   align-items: center;
   color: #8c8aa7;
 `
-const ShareGroup=styled.div`
+const ShareGroup = styled.div`
   display: flex;
   padding: 8px;
   color: white;
   background: #104D92;
   border-radius: 5px;
   cursor: pointer;
+`
+const ReviewsButtonContainer = styled.div`
+  margin: 10px auto;
+  button{
+    margin: 5px auto;
+  }
 `
 
 const ReviewCard = () => {
@@ -80,9 +88,9 @@ const ReviewCard = () => {
         </ReviewCardContainer>
     );
 };
-const Reviews=()=>{
+const Reviews = () => {
     return (
-        <div style={{flex:'0.6', margin:"0 auto"}}>
+        <div style={{flex: '0.6', margin: "0 auto"}}>
             <TagsWrapper>
                 <TagsHeader>Tags</TagsHeader>
                 <TagsContainer>
@@ -95,11 +103,15 @@ const Reviews=()=>{
                     <div className="tag">Replacement</div>
                 </TagsContainer>
             </TagsWrapper>
-            <Scrollbar style={{height:"450px",width:"100%"}}>
-                <ReviewCard/>
-                <ReviewCard/>
-                <ReviewCard/>
-            </Scrollbar>
+            {/*<Scrollbar style={{height:"450px",width:"100%"}}>*/}
+            <ReviewCard/>
+            <ReviewCard/>
+            <ReviewCard/>
+            <ReviewCard/>
+            <ReviewsButtonContainer>
+                <Button type={'button'}>Show More reviews</Button>
+            </ReviewsButtonContainer>
+            {/*</Scrollbar>*/}
         </div>
     )
 }

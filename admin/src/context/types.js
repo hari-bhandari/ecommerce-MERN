bcdf6e9a3305763e8ed1f0cfcb6f12a93290f7d4
Hari Bhandari <2012bhandari.ha@gmail.com>
2021-07-13 18:47:01 +0100

    backend code

--- admin/src/context/types.js
+++
@@ -1,3 +0,0 @@
-export const USER_LOADED = 'USER_LOADED';
-export const LOGOUT = 'LOGOUT';
-export const AUTH_ERROR_GET_ME='AUTH_ERROR_GET_ME';
--- client/next.config.js
+++
@@ -1,7 +0,0 @@
-const withPlugins = require('next-compose-plugins');
-const withOptimizedImages = require('next-optimized-images');
-
-// next.js configuration
-const nextConfig = {};
-
-module.exports = withPlugins([withOptimizedImages], nextConfig);

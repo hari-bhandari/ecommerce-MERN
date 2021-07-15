3df398d887d75c2d916f95619a981a15ad081d90
Hari Bhandari <2012bhandari.ha@gmail.com>
2021-07-15 10:05:40 +0100

    added filter by title function

--- client/src/utils/filterQueries.tsx
+++ client/src/utils/filterQueries.tsx
@@ -3,10 +3,16 @@ import {filterTypes} from "@/components/FilterProducts";
 export const filterProducts=(list:[any],type)=>{
     console.log(type)
     switch (type){
-        case filterTypes.ascendingPrice:
+        case filterTypes.ascendingPrice://sorts array by price --ascending
             return list.sort((a, b) => (a.price - b.price))
-        case filterTypes.descendingPrice:
+        case filterTypes.descendingPrice: //sorts array by price --descending
             return list.sort((a, b) => (b.price-a.price))
+        case filterTypes.alphabetical: //sorts array by title
+            return list.sort(function(a, b){
+                if(a.name < b.name) { return -1; }
+                if(a.name > b.name) { return 1; }
+                return 0;
+            })
         default:
             return list
 

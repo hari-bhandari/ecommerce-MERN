eab30781abfaacb58a5f6f97aebe889d528de6b8
Hari Bhandari <2012bhandari.ha@gmail.com>
2021-07-15 21:01:01 +0100

    added reviews controller functions

--- server/routes/controllers/reviews.js
+++ server/routes/controllers/reviews.js
@@ -1,71 +1,124 @@
-const asyncHandler = require('../../middleware/async')
-const Product = require('../../models/product')
-// @desc    Fetch single product
-// @route   GET /api/products/:id
-// @access  Public
+const ErrorResponse = require('../utils/errorResponse');
+const asyncHandler = require('../middleware/async');
+const Review = require('../../models/review');
+const Product = require('../../models/product');
 
-const getProductReviewsById = asyncHandler(async (req, res) => {
-    const product = await Product.findOne({id: req.params.id}).populate({
-        path:'Category',
-        select:'name id'
-    })
-    if (product) {
-        sendRes(res,200,product)
+// @desc      Get reviews
+// @route     GET /api/v1/reviews
+// @route     GET /api/v1/products/:productId/reviews
+// @access    Public
+exports.getReviews = asyncHandler(async (req, res, next) => {
+    if (req.params.productId) {
+        const reviews = await Review.find({ product: req.params.productId });
+
+        return res.status(200).json({
+            success: true,
+            count: reviews.length,
+            data: reviews
+        });
     } else {
-        res.status(404)
-        throw new Error('Product not found')
+        res.status(200).json(res.advancedResults);
     }
-})
+});
 
-// @desc    Delete a product
-// @route   DELETE /api/products/:id
-// @access  Private/Admin
-const deleteProduct = asyncHandler(async (req, res) => {
-    const product = await Product.findById(req.params.id)
+// @desc      Get single review
+// @route     GET /api/v1/reviews/:id
+// @access    Public
+exports.getReview = asyncHandler(async (req, res, next) => {
+    const review = await Review.findById(req.params.id).populate({
+        path: 'product',
+        select: 'name description'
+    });
 
-    if (product) {
-        await product.remove()
-        res.json({message: 'Product removed'})
-    } else {
-        res.status(404)
-        throw new Error('Product not found')
+    if (!review) {
+        return next(
+            new ErrorResponse(`No review found with the id of ${req.params.id}`, 404)
+        );
     }
-})
-// @desc    Create a product
-// @route   POST /api/products
-// @access  Private/Admin
-const createProduct = asyncHandler(async (req, res) => {
-    const {
-        name,
-        price,
-        description,
-        images,
-        thumbImage,
-        subCategory,
-        category,
-        countInStock,
-    } = req.body
-    const product = new Product({
-        name: name,
-        price: price,
-        user: req.user._id,
-        images: images,
-        thumbImage: thumbImage,
-        subCategory: subCategory,
-        category: category,
-        countInStock: countInStock,
-        numReviews: 0,
-        description: description,
-    })
 
+    res.status(200).json({
+        success: true,
+        data: review
+    });
+});
 
-    const createdProduct = await product.save()
-        sendRes(res,201,createdProduct)
-})
+// @desc      Add review
+// @route     POST /api/v1/products/id/reviews
+// @access    Private
+exports.addReview = asyncHandler(async (req, res, next) => {
+    req.body.product = req.params.id;
+    req.body.user = req.user.id;
 
+    const product = await Product.findById(req.params.id);
 
-module.exports = {
-    getProducts, getProductById,
-    deleteProduct,
-    createProduct
-}
+    if (!product) {
+        return next(
+            new ErrorResponse(
+                `No product with the id of ${req.params.id}`,
+                404
+            )
+        );
+    }
+
+    const review = await Review.create(req.body);
+
+    res.status(201).json({
+        success: true,
+        data: review
+    });
+});
+
+// @desc      Update review
+// @route     PUT /api/v1/reviews/:id
+// @access    Private
+exports.updateReview = asyncHandler(async (req, res, next) => {
+    let review = await Review.findById(req.params.id);
+
+    if (!review) {
+        return next(
+            new ErrorResponse(`No review with the id of ${req.params.id}`, 404)
+        );
+    }
+
+    // Make sure review belongs to user or user is admin
+    if (review.user.toString() !== req.user.id && req.user.role !== 'admin') {
+        return next(new ErrorResponse(`Not authorized to update review`, 401));
+    }
+
+    review = await Review.findByIdAndUpdate(req.params.id, req.body, {
+        new: true,
+        runValidators: true
+    });
+
+    review.save();
+
+    res.status(200).json({
+        success: true,
+        data: review
+    });
+});
+
+// @desc      Delete review
+// @route     DELETE /api/v1/reviews/:id
+// @access    Private
+exports.deleteReview = asyncHandler(async (req, res, next) => {
+    const review = await Review.findById(req.params.id);
+
+    if (!review) {
+        return next(
+            new ErrorResponse(`No review with the id of ${req.params.id}`, 404)
+        );
+    }
+
+    // Make sure review belongs to user or user is admin
+    if (review.user.toString() !== req.user.id && req.user.role !== 'admin') {
+        return next(new ErrorResponse(`Not authorized to update review`, 401));
+    }
+
+    await review.remove();
+
+    res.status(200).json({
+        success: true,
+        data: {}
+    });
+});

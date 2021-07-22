853f5c9c086e75e09ddc707b51535718a86eacd7
Hari Bhandari <2012bhandari.ha@gmail.com>
2021-07-22 19:16:51 +0100

    added few more icons and implemented them

--- client/src/components/profile/Profile.styles.tsx
+++ client/src/components/profile/Profile.styles.tsx
@@ -2,6 +2,65 @@ import styled from "styled-components";
 
 export const ProfileContainer=styled.div`
   margin: 100px auto;
-  max-width:1000px;
+  width: 90%;
+  max-width: 1000px;
   background: white;
+  //react-tabs css
+  .react-tabs {
+    -webkit-tap-highlight-color: transparent;
+  }
+
+  .react-tabs__tab-list {
+    border-bottom: 2px solid #0c0c0c;
+    margin: 0 0 10px;
+    padding: 0;
+  }
+
+  .react-tabs__tab {
+    display: inline-block;
+    border: 2px solid transparent;
+    border-bottom: none;
+    bottom: -2px;
+    position: relative;
+    list-style: none;
+    padding: 6px 12px;
+    cursor: pointer;
+  }
+
+  .react-tabs__tab--selected {
+    background: #21C5C5;
+    border-color: #000000;
+    color: white;
+    border-radius: 5px 5px 0 0;
+  }
+
+  .react-tabs__tab--disabled {
+    color: GrayText;
+    cursor: default;
+  }
+
+  .react-tabs__tab:focus {
+    box-shadow: 0 0 5px hsl(208, 99%, 50%);
+    border-color: hsl(208, 99%, 50%);
+    outline: none;
+  }
+
+  .react-tabs__tab:focus:after {
+    content: "";
+    position: absolute;
+    height: 5px;
+    left: -4px;
+    right: -4px;
+    bottom: -5px;
+    background: #fff;
+  }
+
+  .react-tabs__tab-panel {
+    display: none;
+  }
+
+  .react-tabs__tab-panel--selected {
+    display: block;
+  }
+
 `
--- client/src/components/profile/Profile.tsx
+++ client/src/components/profile/Profile.tsx
@@ -1,20 +1,32 @@
 import React from 'react';
 import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
-import 'react-tabs/style/react-tabs.css';
 import {ProfileContainer} from "@/components/profile/Profile.styles";
+import styled from "styled-components";
+import UserIcon from "@/assets/icons/UserIcon";
+import OrdersIcon from "@/assets/icons/OrdersIcon";
+import ReviewsIcon from "@/assets/icons/ReviewsIcon";
+const TabListHeader=styled.p`
+  font-size: 18px;
+  font-weight: 700;
+  line-height: 1.2em;
+  font-family: Lato,sans-serif;
+`
 const Profile = () => {
     return (
         <ProfileContainer>
         <Tabs>
             <TabList>
-                <Tab>My Profile</Tab>
-                <Tab>My orders</Tab>
-                <Tab>My Reviews</Tab>
-
+                <Tab><TabListHeader><UserIcon/> { ' '}My Profile</TabListHeader></Tab>
+                <Tab><TabListHeader><OrdersIcon/>My Orders</TabListHeader></Tab>
+                <Tab><TabListHeader><ReviewsIcon/>My Reviews</TabListHeader></Tab>
             </TabList>
 
             <TabPanel>
                 <h2>Any content 1</h2>
+
+            </TabPanel>
+            <TabPanel>
+                <h2>Any content 2</h2>
             </TabPanel>
             <TabPanel>
                 <h2>Any content 2</h2>

faf87dfcd0e98091f87694b946fa29e65ae324b4
Hari Bhandari <2012bhandari.ha@gmail.com>
2021-07-25 19:11:57 +0100

    added buttons -delete and remove

--- client/src/components/Product/details/Review/Reviews.tsx
+++ client/src/components/Product/details/Review/Reviews.tsx
@@ -7,16 +7,16 @@ import {Button} from "@/components/Others/button/button";
 import {themeGet} from "@styled-system/theme-get";
 import {OpenReviewsTab} from "@/OpenModalFunctions";
 import dateFormat from 'dateformat'
-import { useRouter} from "next/router";
+import {useRouter} from "next/router";
 
-const ReviewCardContainer = styled.div<{modal:boolean}>`
-  margin: ${props => !props.modal?'2px':'0 auto'};
+const ReviewCardContainer = styled.div<{ modal: boolean }>`
+  margin: ${props => !props.modal ? '2px' : '0 auto'};
   margin-bottom: 0;
   display: flex;
   flex-direction: column;
   justify-content: space-around;
   max-width: 709px;
-  width:  ${props => props.modal?'90%':'100%'};
+  width: ${props => props.modal ? '90%' : '100%'};
   max-height: 250px;
   padding: 8px 8px;
   background: rgba(255, 255, 255, 0.4);
@@ -49,11 +49,12 @@ const NameGroup = styled.div`
   display: flex;
   text-overflow: ellipsis;
   color: ${themeGet('colors.primary.light', '#009e7f')};
-  p{
+
+  p {
     text-overflow: ellipsis;
     width: 75px;
-    overflow:hidden;
-    white-space:nowrap;
+    overflow: hidden;
+    white-space: nowrap;
   }
 `
 const Initials = styled.div`
@@ -66,34 +67,39 @@ const Initials = styled.div`
   border-radius: 50%;
   background: #d56a6a;
 `
-const ProductImageContainer=styled.div`
+const ProductImageContainer = styled.div`
   flex: 0.3;
-  height:60px ;
+  height: 60px;
   width: 60px;
-  img{
-    height:60px ;
+
+  img {
+    height: 60px;
     width: 60px;
   }
 `
-const ProductTitleContainer=styled.div`
-    flex: 0.7;
-  h4{
+const ProductTitleContainer = styled.div`
+  flex: 0.7;
+
+  h4 {
     font-size: 18px;
   }
-  h4:hover{
+
+  h4:hover {
     color: #00a8ff;
   }
-    
+
 `
 
 const Description = styled.div`
   display: flex;
   flex-direction: column;
-  b{
+
+  b {
     text-align: left;
     font-size: larger;
     color: ${themeGet('colors.primary.regular', '#009e7f')};
   }
+
   color: #2b2b2b;
   font-weight: 400;
   margin: 0px 45px;
@@ -105,55 +111,83 @@ const Details = styled.div`
   align-items: center;
   color: #8c8aa7;
 `
-const ShareGroup = styled.div`
+const ShareGroup = styled.div<{delete?:boolean}>`
   display: flex;
   padding: 8px;
   color: white;
-  background: #104D92;
+  margin-right: 2px;
+  background:  ${props => props.delete ? '#a12f2f' : '#104D92'};
   border-radius: 5px;
   cursor: pointer;
+
+`
+const EditButtonsContainer=styled.div`
+  display: flex;
 `
 const ReviewsButtonContainer = styled.div`
   margin: 10px auto;
-  button{
+
+  button {
     margin: 5px auto;
   }
 `
 
-export const ReviewCard:React.FC<{name:string,comment:string,rating:number,product?:any,title:string,myReview?:boolean,modal?:boolean,createdAt:any}> = ({name,createdAt,product,myReview,comment,modal,rating,title}) => {
-    const created=new Date(createdAt)
-    const createdDate=dateFormat(created,"dddd, mmmm dS, yyyy")
-    const router=useRouter()
-    const onClick=()=>{
-        if(product.id){
+export const ReviewCard: React.FC<{ name: string, comment: string, rating: number, product?: any, title: string, myReview?: boolean, modal?: boolean, createdAt: any }> = ({
+                                                                                                                                                                               name,
+                                                                                                                                                                               createdAt,
+                                                                                                                                                                               product,
+                                                                                                                                                                               myReview,
+                                                                                                                                                                               comment,
+                                                                                                                                                                               modal,
+                                                                                                                                                                               rating,
+                                                                                                                                                                               title
+                                                                                                                                                                           }) => {
+    const created = new Date(createdAt)
+    const createdDate = dateFormat(created, "dddd, mmmm dS, yyyy")
+    const router = useRouter()
+    const onClick = () => {
+        if (product.id) {
             router.push(`/product/${product.id}`)
         }
     }
     return (
         <div>
-        <ReviewCardContainer modal={modal} >
-            <ReviewHeader>
-                <NameGroup>
-                    <Initials>{name.slice(0,1)}</Initials>
-                    <p>{name}</p>
-                </NameGroup>
-                <StarRating rating={rating}/>
-            </ReviewHeader>
-            <Description>
-                <b>{title}</b>
-                {comment}
-            </Description>
-            <Details>
-                <div className="review-date">{createdDate}</div>
-                <ShareGroup>
-                    <p>Helpful</p>
-                </ShareGroup>
+            <ReviewCardContainer modal={modal}>
+                <ReviewHeader>
+                    <NameGroup>
+                        <Initials>{name.slice(0, 1)}</Initials>
+                        <p>{name}</p>
+                    </NameGroup>
+                    <StarRating rating={rating}/>
+                </ReviewHeader>
+                <Description>
+                    <b>{title}</b>
+                    {comment}
+                </Description>
+                <Details>
+                    <div className="review-date">{createdDate}</div>
+
+                    {product ?
+                        <>
+                            <EditButtonsContainer>
+                            <ShareGroup delete={true}>
+                                <p>Delete</p>
+                            </ShareGroup>
+                            <ShareGroup>
+                                <p>Edit</p>
+                            </ShareGroup>
+                            </EditButtonsContainer>
+                        </> :
+                        <ShareGroup>
+                            <p>Helpful</p>
+                        </ShareGroup>
+                    }
             </Details>
             {product &&
             <ProductContainer onClick={onClick}>
                 <ProductImageContainer>
                     <img src={product.thumbImage}
-                         alt={product.name+ ' image'}/>
+                         alt={product.name + ' image'}/>
                 </ProductImageContainer>
                 <ProductTitleContainer>
                     <h4>{product.name}</h4>
@@ -164,34 +198,36 @@ export const ReviewCard:React.FC<{name:string,comment:string,rating:number,produ
 
         </ReviewCardContainer>
 
-        </div>
-    );
+</div>
+);
 };
-const Reviews:React.FC<{reviews:any,modal?:boolean}> = ({reviews,modal}) => {
-    const loadMore=reviews.length>3?true:false
-    const [selected,setSelected]=useState<number|null>(null)
-    const filteredReviews=()=>{
-        if(!selected){
+const Reviews: React.FC<{ reviews: any, modal?: boolean }> = ({reviews, modal}) => {
+    const loadMore = reviews.length > 3 ? true : false
+    const [selected, setSelected] = useState<number | null>(null)
+    const filteredReviews = () => {
+        if (!selected) {
             return reviews
         }
-        if(selected>0){
-            return reviews.filter((data)=>data.rating===selected)
+        if (selected > 0) {
+            return reviews.filter((data) => data.rating === selected)
         }
     }
-    const isSelected=(number:number)=>{
-        return number===selected?'selected':''
+    const isSelected = (number: number) => {
+        return number === selected ? 'selected' : ''
     }
-    const onClickForStars=(number:number)=>{
+    const onClickForStars = (number: number) => {
         setSelected(number)
     }
-    const starBlocks=['⭐','⭐⭐','⭐⭐⭐','⭐⭐⭐⭐','⭐⭐⭐⭐⭐']
+    const starBlocks = ['⭐', '⭐⭐', '⭐⭐⭐', '⭐⭐⭐⭐', '⭐⭐⭐⭐⭐']
     return (
         <div style={{flex: '0.6', margin: "0 auto"}}>
             <TagsWrapper>
                 <TagsContainer>
-                    { starBlocks.map(data=>{
+                    {starBlocks.map(data => {
                         return (
-                            <div className={`tag ${isSelected(data.length)}`} onClick={()=>{onClickForStars(data.length)}}>{data}</div>
+                            <div className={`tag ${isSelected(data.length)}`} onClick={() => {
+                                onClickForStars(data.length)
+                            }}>{data}</div>
                         )
                     })
                     }
@@ -200,16 +236,22 @@ const Reviews:React.FC<{reviews:any,modal?:boolean}> = ({reviews,modal}) => {
                 </TagsContainer>
             </TagsWrapper>
             {/*<Scrollbar style={{height:"450px",width:"100%"}}>*/}
-            {modal?<div><Scrollbar style={{height:"450px",width:"100%"}}>
-                    {filteredReviews().length===0&&<h4>No reviews found with {selected} stars</h4>}
-                    {filteredReviews().map(({name,comment,rating,title,createdAt})=>(<ReviewCard name={name} comment={comment} title={title} rating={rating} modal={true} createdAt={createdAt}/>))}
-                </Scrollbar></div>:
+            {modal ? <div><Scrollbar style={{height: "450px", width: "100%"}}>
+                    {filteredReviews().length === 0 && <h4>No reviews found with {selected} stars</h4>}
+                    {filteredReviews().map(({name, comment, rating, title, createdAt}) => (
+                        <ReviewCard name={name} comment={comment} title={title} rating={rating} modal={true}
+                                    createdAt={createdAt}/>))}
+                </Scrollbar></div> :
                 <>
-                    {filteredReviews().slice(0,3).map(({comment,rating,title,name,createdAt})=>(<ReviewCard name={name} comment={comment} title={title} rating={rating} createdAt={createdAt} />))}
-                    {filteredReviews().length===0&&<h4>No reviews with {selected} stars</h4>}
+                    {filteredReviews().slice(0, 3).map(({comment, rating, title, name, createdAt}) => (
+                        <ReviewCard name={name} comment={comment} title={title} rating={rating}
+                                    createdAt={createdAt}/>))}
+                    {filteredReviews().length === 0 && <h4>No reviews with {selected} stars</h4>}
                     {loadMore &&
                     <ReviewsButtonContainer>
-                        <Button type={'button'} onClick={()=>{OpenReviewsTab(reviews)}}>see all reviews</Button>
+                        <Button type={'button'} onClick={() => {
+                            OpenReviewsTab(reviews)
+                        }}>see all reviews</Button>
                     </ReviewsButtonContainer>
                     }
                 </>}

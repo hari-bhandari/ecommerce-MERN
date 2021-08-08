304e344ae9e16ee0282330bc4dd41e9e837d9c9e
Hari Bhandari <2012bhandari.ha@gmail.com>
2021-08-08 19:24:29 +0100

    made img format optional

--- client/src/utils/config.ts
+++ client/src/utils/config.ts
@@ -1,5 +1,5 @@
 export const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL
-export const transformCloudinaryImage = (url, width, height, imgFormat) => {
+export const transformCloudinaryImage = (url:string, width:number, height:number, imgFormat?:string) => {
     // https://res.cloudinary.com/wisecart/image/upload/w_150,h_300,c_fill/v1617441971/re3lf0beoxdrw4hdqbeb.png
     let [first, last] = url.split('/upload/') //splits the url at /upload/ and returns an array
     if (imgFormat) {

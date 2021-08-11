75620356fdc0c10ba241742f39a44311e54a8aa5
hari-bhandari <2012bhandari.ha@gmail.com>
2021-08-11 17:58:38 +0100

    some more adjustments to _document.tsx

--- client/src/pages/_document.tsx
+++ client/src/pages/_document.tsx
@@ -1,21 +1,14 @@
-import Document, {DocumentContext, Head, Html, Main, NextScript} from 'next/document';
+import Document, {Head, Html, Main, NextScript} from 'next/document';
 import {ServerStyleSheet} from 'styled-components';
 
 class MyDocument extends Document {
-    static async getInitialProps(ctx: DocumentContext) {
+    static async getInitialProps({renderPage}) {
         const sheet = new ServerStyleSheet();
-        const originalRenderPage = ctx.renderPage;
         try {
-            ctx.renderPage = () =>
-                originalRenderPage({
-                    enhanceApp: (App) => (props: any) =>
-                        sheet.collectStyles(<App {...props} />),
-                });
-
-            const initialProps = await Document.getInitialProps(ctx);
-            return {...initialProps, styles: sheet.getStyleElement()};
-
-
+            const page = renderPage(App => props =>
+                sheet.collectStyles(<App {...props} />)
+            );
+            return {...page, styles: sheet.getStyleElement()};
         } finally {
             sheet.seal()
         }

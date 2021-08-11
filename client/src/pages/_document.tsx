e02695947113af3c5554b33584595175dbd36f28
hari-bhandari <2012bhandari.ha@gmail.com>
2021-08-11 18:16:28 +0100

    made some changes on _document.tsx once again

--- client/src/pages/_document.tsx
+++ client/src/pages/_document.tsx
@@ -1,19 +1,19 @@
-import Document, {DocumentContext, Head, Html, Main, NextScript,} from 'next/document';
-import {ServerStyleSheet} from 'styled-components';
+import Document from 'next/document'
+import {ServerStyleSheet} from 'styled-components'
 
-export default class CustomDocument extends Document {
-    static async getInitialProps(ctx: DocumentContext) {
-        const sheet = new ServerStyleSheet();
-        const originalRenderPage = ctx.renderPage;
+export default class MyDocument extends Document {
+    static async getInitialProps(ctx) {
+        const sheet = new ServerStyleSheet()
+        const originalRenderPage = ctx.renderPage
 
         try {
             ctx.renderPage = () =>
                 originalRenderPage({
-                    enhanceApp: (App) => (props: any) =>
+                    enhanceApp: (App) => (props) =>
                         sheet.collectStyles(<App {...props} />),
-                });
+                })
 
-            const initialProps = await Document.getInitialProps(ctx);
+            const initialProps = await Document.getInitialProps(ctx)
             return {
                 ...initialProps,
                 styles: (
@@ -22,27 +22,9 @@ export default class CustomDocument extends Document {
                         {sheet.getStyleElement()}
                     </>
                 ),
-            };
+            }
         } finally {
-            sheet.seal();
+            sheet.seal()
         }
-    }
-
-    render() {
-        return (
-            <Html lang='en'>
-                <Head>
-                    <meta
-                        name='viewport'
-                        content='width=device-width, initial-scale=1.0, maximum-scale=1.0'
-                    />
-                </Head>
-                <body>
-                <Main/>
-                <div id='modal-root'></div>
-                <NextScript/>
-                </body>
-            </Html>
-        );
     }
 }

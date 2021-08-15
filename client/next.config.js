68e8cf41669693fef18c826f957b215820571377
hari-bhandari <2012bhandari.ha@gmail.com>
2021-08-15 19:08:36 +0100

    turned back to react -- preact seems laggy

--- client/next.config.js
+++ client/next.config.js
@@ -1,16 +1,12 @@
 const withPlugins = require('next-compose-plugins');
 const withOptimizedImages = require('next-optimized-images');
-const withPrefresh = require("@prefresh/next");
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
     enabled: process.env.ANALYZE === 'true',
 })
-const withPreact = require('next-plugin-preact');
-
 const nextConfig = {
     images: {
         domains: ['res.cloudinary.com'],
     },
 }
-
 // next.js configuration
-module.exports = withPlugins([withPreact({}), withBundleAnalyzer,], nextConfig);
+module.exports = withPlugins([withBundleAnalyzer, withOptimizedImages], nextConfig);

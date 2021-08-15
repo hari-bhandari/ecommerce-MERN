f40026c61a7498b392f74d77447e325430ad1337
hari-bhandari <2012bhandari.ha@gmail.com>
2021-08-15 19:02:27 +0100

    turned back to react -- issues once again

--- client/next.config.js
+++ client/next.config.js
@@ -1,20 +1,16 @@
 const withPlugins = require('next-compose-plugins');
 const withOptimizedImages = require('next-optimized-images');
+const withPrefresh = require("@prefresh/next");
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
     enabled: process.env.ANALYZE === 'true',
 })
-
-const withPrefresh = require('@prefresh/next');
-const preact = require('preact');
 const withPreact = require('next-plugin-preact');
+
 const nextConfig = {
     images: {
         domains: ['res.cloudinary.com'],
     },
 }
+
 // next.js configuration
-module.exports = withPlugins([withPreact({
-    experimental: {
-        modern: true,
-    },
-}), withBundleAnalyzer, withOptimizedImages], nextConfig);
+module.exports = withPlugins([withPreact({}), withBundleAnalyzer,], nextConfig);

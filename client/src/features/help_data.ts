b63b027ed12a675f3e6d81a8ebb9067626de0304
Hari Bhandari <2012bhandari.ha@gmail.com>
2022-06-15 11:11:29 +0100

    added help section

---
+++ client/src/features/help_data.ts
@@ -0,0 +1,28 @@
+export const FAQ_DATA = [
+    {
+        id: 1,
+        question: 'What is the purpose of this website?',
+        answer: "The purpose of this website is to help you find the best deals on your mobile phones, we compare deals from all providers and give you the best deal for your phone. We also provide you with a list of all the providers that we support."
+    },
+    {
+        id: 2,
+        question: "Are there any hidden fees?",
+        answer: "No, we do not charge any fees for using this website. We do however get a small commission from each provider that we support."
+    },
+    {
+        id: 3,
+        question: "Can i return a phone/sim?",
+        answer: "Yes, you can return a phone/sim if you are not satisfied with the deal you received. We will refund the money to the provider that you purchased the phone/sim from."
+    },
+    {
+        id: 4,
+        question: "Can i change my mind and buy a different phone?",
+        answer: "Yes, you can change your mind and buy a different phone. We will refund the money to the provider that you purchased the phone/sim from."
+    },
+    {
+        id: 5,
+        question: "Is my data safe?",
+        answer: "Yes, your data is safe with us and your passwords are encrypted. You data will not be shared with any third parties."
+    }
+
+];
